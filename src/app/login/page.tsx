ba9e18299c565a3b5095524f7e1bb5966f28f06b
Nava-stack <129419033+Nava-stack@users.noreply.github.com>
2024-09-22 12:37:56 +0530

    Add bcryptjs, jsonwebtoken, nodemailer, and react-hot-toast dependencies

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,34 +1,52 @@
 "use client";
-
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
 import React from "react";
 
-const login = () => {
+export default function LoginPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const handleLogin = async () => {
+    try {
+    } catch (error) {
+      alert("Login failed");
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="mb-6 text-2xl font-bold">Login</h1>
-      <form className="flex flex-col w-80 p-6 border rounded-lg shadow-md text-center">
+      <form className="flex flex-col w-80 p-6 border rounded-md shadow-md text-center">
         <input
-          type="text"
-          placeholder="Username"
+          type="email"
+          placeholder="Email"
           className="mb-4 p-2 border border-gray-300 rounded"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
         />
         <input
           type="password"
           placeholder="Password"
           className="mb-6 p-2 border border-gray-300 rounded"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
         />
         <button
           type="submit"
           className="p-2 bg-white text-black font-semibold rounded hover:bg-gray-600 hover:text-white"
+          onClick={handleLogin}
         >
           Login
         </button>
-        <a href="/register" className="mt-5">
-          register here
-        </a>
+        <Link href="/register" className="mt-5">
+          Are you a new user?
+        </Link>
       </form>
     </div>
   );
-};
-
-export default login;
+}
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,64 @@
 "use client";
-
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
 import React from "react";
 
-const register = () => {
-  return <div>register</div>;
-};
+export default function RegisterPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    username: "",
+    password: "",
+  });
 
-export default register;
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post("/api/register", user);
+      if (response.status === 200) {
+        alert("Register success");
+      }
+    } catch (error) {
+      alert("Register failed");
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="mb-6 text-2xl font-bold">Register</h1>
+      <form className="flex flex-col w-80 p-6 border rounded-md shadow-md text-center">
+        <input
+          type="email"
+          placeholder="Email"
+          className="mb-4 p-2 border border-gray-300 rounded"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          className="mb-4 p-2 border border-gray-300 rounded"
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="mb-6 p-2 border border-gray-300 rounded"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+        <button
+          type="submit"
+          className="p-2 bg-white text-black font-semibold rounded hover:bg-gray-600 hover:text-white"
+          onClick={handleRegister}
+        >
+          Signup
+        </button>
+        <Link href="/login" className="mt-5">
+          Already have an account?
+        </Link>
+      </form>
+    </div>
+  );
+}
