33a335b3e26dafb4268af3608cecfae6682027ab
Nava-stack <129419033+Nava-stack@users.noreply.github.com>
2024-09-23 13:34:26 +0530

    Add logout API route and implement logout logic

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
 export default function ProfilePage() {
+  const router = useRouter();
+  const handleLogout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1>Profile Page</h1>
+      <p className="text-4xl">Profile Page</p>
+      <hr />
+      <button
+        onClick={handleLogout}
+        className="bg-white text-black text-2xl px-4 py-2 rounded-md"
+      >
+        Logout
+      </button>
     </div>
   );
 }
