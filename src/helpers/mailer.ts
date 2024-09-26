4b1c2fece6fd3f8c67ba1d89f088c776aa42909f
Nava-stack <129419033+Nava-stack@users.noreply.github.com>
2024-09-26 20:11:20 +0530

    Refactor email sending logic and add SMTP configuration

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -2,7 +2,13 @@ import nodemailer from "nodemailer";
 import User from "@/models/userModel";
 import bcryptjs from "bcryptjs";
 
-export const sendEmail = async ({ email, emailType, userId }: any) => {
+interface Params {
+  email: string;
+  emailType: string;
+  userId: string;
+}
+
+export const sendEmail = async ({ email, emailType, userId }: Params) => {
   try {
     // * Create a hashed Token
     const hashedToken = await bcryptjs.hash(userId.toString(), 10);
@@ -29,6 +35,25 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
       },
     });
 
+    // * Define email options
+    const mailOptions = {
+      from: process.env.SMTP_USER_MAIL,
+      to: email,
+      subject: emailType === "VERIFY" ? "Verify Email" : "Reset Password",
+      html: `
+        <h1>Click the link below to ${
+          emailType === "VERIFY" ? "verify your Email" : "reset your Password"
+        }</h1>
+        <p><a href="${
+          process.env.DOMAIN
+        }/verifyemail?token=${hashedToken}">Here</a></p>
+      `,
+    };
+
+    // * Send email
+    const mailResponse = await transporter.sendMail(mailOptions);
+    return mailResponse;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     throw new Error(error.message);
   }
