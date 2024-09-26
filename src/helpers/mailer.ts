0654266293fe7cada053f150b7380917b736dafd
Nava-stack <129419033+Nava-stack@users.noreply.github.com>
2024-09-26 19:42:28 +0530

    Refactor email sending logic and add SMTP configuration

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,3 +1,35 @@
 import nodemailer from "nodemailer";
 import User from "@/models/userModel";
 import bcryptjs from "bcryptjs";
+
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    // * Create a hashed Token
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 10 * 60 * 1000,
+      });
+    } else if (emailType === "FORGOT") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 10 * 60 * 1000,
+      });
+    }
+
+    // * Create a transporter
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT),
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
